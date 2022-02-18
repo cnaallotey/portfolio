e052ecfd551875650106b5281ea44acb1095230f
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-02-18 21:42:19 +0000

    setting up page and navbar

---
+++ src/components/navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="w-full">
+      <div class="max-w-screen-xl mx-auto px-5 py-5 xl:px-0">
+        <div class="w-full flex items-center justify-between">
+          <p>Charles.</p>
+          <div>
+            <ul class="inline-flex space-x-8 text-base font-medium text-gray-800">
+              <li>About me</li>
+              <li>Services</li>
+              <li>Works</li>
+              <li>Skills</li>
+            </ul>
+          </div>
+          <button
+            class="py-3 px-4 text-base font-medium text-white bg-rose-500 hover:bg-rose-600 rounded-md shadow-lg"
+          >
+            Contact me
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
