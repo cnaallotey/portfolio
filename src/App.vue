c2110cfafe11b0333db253eab5b23e131abc6a3a
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-02-18 23:24:48 +0000

    hero image

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import navbar from './components/navbar.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import navbar from "./components/navbar.vue";
+import hero from "./components/hero.vue";
 </script>
 
 <template>
-  <navbar/>
+  <navbar />
+  <hero />
 </template>
---
+++ src/components/hero.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="w-full">
+      <div class="max-w-screen-xl mx-auto px-5 xl:px-0">
+        <div class="w-full relative flex py-20 lg:py-40 flex-col items-center">
+          <img
+            src="../assets/29 6.png"
+            class="w-16 rounded-full bg-orange-200"
+            alt="emogi"
+          />
+          <p class="text-base font-normal md:text-xl text-gray-800 mt-10">
+            Hello, i'm <span class="font-bold">Charles Allotey</span>🥰
+          </p>
+          <h1
+            class="text-6xl max-w-3xl mt-4 text-center md:text-6xl lg:text-7xl text-slate-900 font-extrabold"
+          >
+            Building Products, Brands & Experiences
+          </h1>
+          <p
+            class="text-base relative z-0 text-center font-normal md:text-base text-gray-800 mt-10"
+          >
+            Charles is a Frontend Engineer , UI Designer and Graphic Designer
+          </p>
+          <div
+            class="imagetrim absolute lg:flex hidden -z-10 top-10 left-0 overflow-hidden bg-orange-200"
+          >
+            <img
+              src="../assets/african-american-student-walking-street-talking-phone.png"
+              class="w-96"
+              alt=""
+            />
+          </div>
+          <div
+            class="imagetrims hidden lg:flex absolute -z-10 bottom-10 right-0 overflow-hidden bg-rose-200"
+          >
+            <img
+              src="../assets/young-attractive-handsome-guy-feels-delighted-gladden-amazed.png"
+              class="w-96"
+              alt=""
+            />
+          </div>
+          <button
+            class="px-4 py-3 flex items-center bg-slate-800 shadow-lg mt-5 text-white text-base font-medium rounded-lg"
+          >
+            <span
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                data-name="Layer 1"
+                viewBox="0 0 24 24"
+                class="w-8 h-8 mr-2"
+                fill="#ffffff"
+              >
+                <path
+                  d="M12,2.2467A10.00042,10.00042,0,0,0,8.83752,21.73419c.5.08752.6875-.21247.6875-.475,0-.23749-.01251-1.025-.01251-1.86249C7,19.85919,6.35,18.78423,6.15,18.22173A3.636,3.636,0,0,0,5.125,16.8092c-.35-.1875-.85-.65-.01251-.66248A2.00117,2.00117,0,0,1,6.65,17.17169a2.13742,2.13742,0,0,0,2.91248.825A2.10376,2.10376,0,0,1,10.2,16.65923c-2.225-.25-4.55-1.11254-4.55-4.9375a3.89187,3.89187,0,0,1,1.025-2.6875,3.59373,3.59373,0,0,1,.1-2.65s.83747-.26251,2.75,1.025a9.42747,9.42747,0,0,1,5,0c1.91248-1.3,2.75-1.025,2.75-1.025a3.59323,3.59323,0,0,1,.1,2.65,3.869,3.869,0,0,1,1.025,2.6875c0,3.83747-2.33752,4.6875-4.5625,4.9375a2.36814,2.36814,0,0,1,.675,1.85c0,1.33752-.01251,2.41248-.01251,2.75,0,.26251.1875.575.6875.475A10.0053,10.0053,0,0,0,12,2.2467Z"
+                /></svg
+            ></span>
+            View Github
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.imagetrim {
+  border-radius: 0px 160px 0px 160px;
+}
+.imagetrims {
+  border-radius: 160px 0px 160px 0px;
+}
+</style>
