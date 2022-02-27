0eca56c25f301863d24170512c9808bfcd621ff6
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-02-27 12:26:25 +0000

    add social icons

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -14,69 +14,24 @@
             >
               Get in touch!
             </p>
-            <div class="flex justify-around mt-4 max-w-screen-sm mx-auto">
+            <div class="flex justify-around mt-4 w-fulll mx-auto">
               <div
                 class="p-8 flex flex-col items-center group hover:bg-rose-500 hover:shadow-2xl rounded-2xl transition-all select-none cursor-pointer duration-200"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="feather feather-twitter text-white"
-                >
-                  <path
-                    d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
-                  ></path>
-                </svg>
+                <img src="../assets/twit.svg" class="w-12" alt="" />
                 <p class="text-base text-white font-normal mt-2">@adotey_</p>
               </div>
               <div
                 class="p-8 flex flex-col items-center group hover:bg-rose-500 hover:shadow-2xl rounded-2xl transition-all select-none cursor-pointer duration-200"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="feather feather-twitter text-white skew-x-0"
-                >
-                  <path
-                    d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
-                  ></path>
-                </svg>
-                <p class="text-base text-white font-normal mt-2">@adotey_</p>
+                <img src="../assets/linkedin.svg" class="w-12" alt="" />
+                <p class="text-base text-white font-normal mt-2">Charles Allotey</p>
               </div>
               <div
                 class="p-8 flex flex-col items-center group hover:bg-rose-500 hover:shadow-2xl rounded-2xl transition-all select-none cursor-pointer duration-200 skew-x-0"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="feather feather-twitter text-white"
-                >
-                  <path
-                    d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
-                  ></path>
-                </svg>
-                <p class="text-base text-white font-normal mt-2">@adotey_</p>
+                <img src="../assets/behance.svg" class="w-12" alt="" />
+                <p class="text-base text-white font-normal mt-2">Charles Allotey</p>
               </div>
             </div>
             <div class="w-full bg-white rounded-2xl shadow-lg p-5 lg:p-8 mt-5">
