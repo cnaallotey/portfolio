3998b4b611b24ce401129081165d8cceb6e03f29
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-03-04 11:49:38 +0000

    change cv link

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -6,9 +6,9 @@
           <p class="text-blue-600 font-bold text-2xl">
             Charles<span class="text-rose-500">.</span>
           </p>
-          <div>
+          <div class="flex">
             <ul
-              class="hidden lg:inline-flex space-x-8 text-base font-medium text-gray-800 select-none cursor-pointer"
+              class="hidden space-x-8 text-base font-medium text-gray-800 select-none cursor-pointer"
             >
               <li>About me</li>
               <li>Services</li>
@@ -16,12 +16,24 @@
               <li>Skills</li>
             </ul>
           </div>
-          <button
-            class="py-3 px-4 text-base font-medium text-white bg-rose-500 hover:bg-rose-600 rounded-md shadow-lg"
-            @click="scroll()"
-          >
-            Contact me
-          </button>
+          <div class="flex items-center">
+            <button
+              class="py-3 px-4 text-base font-medium text-white bg-rose-500 hover:bg-rose-600 rounded-md shadow-lg"
+              @click="scroll()"
+            >
+              Contact me
+            </button>
+            <div class="ml-4 flex flex-col items-center border-l-2 border-slate-500 pl-4">
+              <img src="../assets/whatsapp.svg" class="w-4 h-4" alt="whatsapp" />
+              <p class="font-normal text-sm text-slate-900">+233-50-655-7295</p>
+            </div>
+            <div class="ml-4 flex flex-col items-center">
+              <img src="../assets/gmail.svg" class="w-4 h-4" alt="whatsapp" />
+              <p class="font-normal text-sm text-slate-900">
+                charlesallotey1995@gmail.com
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
